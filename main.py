e540a96ad70de6ebb0f6462705a06f19da93bff1
githubfdsh <92286035+githubfdsh@users.noreply.github.com>
2021-10-25 20:10:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+'''
+
+1. Syntax means structure of a program, which necessarily means that every language has certain rules and regulations
+on the structure of the program
+2.
+'''
+
+a = "string1"
+b = "string2"
+
+for i in range(10):
+    print(i)
